294d30a9c55d042defa4a7c917931f4509a32492
rafinha <rafinha72465@gmail.com>
2026-04-04 01:48:56 -0300

    criação da tela de sobre o projeto

--- encryption_playground/lib/features/home/presentation/pages/about_page.dart
+++ encryption_playground/lib/features/home/presentation/pages/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -9,36 +10,117 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About the Project'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(24.0),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/logo_full.png',
-              height: 150,
-              fit: BoxFit.contain,
+
+            /// logo & context
+            Column(
+              children: [
+                Image.asset(
+                  'assets/logo_full.png',
+                  height: 150,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 32),
+                Text(
+                  'Encryption Playground',
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Este projeto foi desenvolvido durante a disciplina de Segurança da Informação, durante o primeiro semestre do ano de 2026, '
+                  'com o intuito de demonstrar o funcionamento dos algoritmos de cripografia',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                Divider(color: Theme.of(context).dividerColor,),
+                const SizedBox(height: 16),
+
+
+
+              ],
             ),
-            const SizedBox(height: 32),
-            Text(
-              'Encryption Playground',
-              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+
+            /// author social buttons
+            Column(
+              children: [
+                Text(
+                  'Contato',
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                     fontWeight: FontWeight.bold,
                   ),
+                ),
+                const SizedBox(height: 16),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    /// github button
+                    OutlinedButton(
+                        onPressed: (){
+                          _openUrl('https://github.com/rafinha-as-br');
+                        },
+                        child: Row(
+                          spacing: 5,
+                          children: [
+                            Icon(Icons.link),
+                            Text('GitHub')
+                          ],
+                        )
+                    ),
+
+                    /// linkedIn button
+                    OutlinedButton(
+                        onPressed: (){
+                          _openUrl('https://www.linkedin.com/in/rafael-antunes-souza/');
+
+                        },
+                        child: Row(
+                          spacing: 5,
+                          children: [
+                            Icon(Icons.link),
+                            Text('LinkedIn')
+                          ],
+                        )
+                    ),
+
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Divider(color: Theme.of(context).dividerColor,),
+                const SizedBox(height: 16),
+              ],
             ),
-            const SizedBox(height: 16),
-            const Text(
-              'A playground for developers to explore encryption and hashing algorithms.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
+
+
+            /// author social buttons
+            Column(
+              children: [
+                Text(
+                  'Sobre o aplicativo',
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                /// project versions & license
+                Column(
+                  children: [
+                    _buildInfoRow(context, 'Version', '1.0.0'),
+                    _buildInfoRow(context, 'Developer', 'Rafael Antunes Souza'),
+                    _buildInfoRow(context, 'License', 'MIT'),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(height: 32),
-            const Divider(),
-            const SizedBox(height: 16),
-            _buildInfoRow(context, 'Version', '1.0.0'),
-            _buildInfoRow(context, 'Developer', 'Rafinha'),
-            _buildInfoRow(context, 'License', 'MIT'),
-            const SizedBox(height: 32),
+
           ],
         ),
       ),
@@ -59,5 +141,13 @@ class AboutPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _openUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw Exception('Não foi possível abrir $url');
+    }
   }
 }
